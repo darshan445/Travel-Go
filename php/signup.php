995316c94d3101b785be62d26428c96d3c2929df
Darshan Thakor <70843532+darshan445@users.noreply.github.com>
2021-03-11 09:08:24 +0530

    Create signup.php

---
+++ php/signup.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="signup.css"/>
+</head>
+<body>
+<?php
+    require('registration2.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['name'])) {
+        // removes backslashes
+        $name = stripslashes($_REQUEST['name']);
+        //escapes special characters in a string
+        $name = mysqli_real_escape_string($conn, $name);
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($conn, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($conn, $password);
+        
+        $confirmpassword = stripslashes($_REQUEST['confirmpassword']);
+        $confirmpassword = mysqli_real_escape_string($conn, $confirmpassword);
+        
+        $query    = "INSERT into `registration` (name,email,password,confirmpassword)
+                     VALUES ('$name','$email','$password','$confirmpassword')";
+        $result   = mysqli_query($conn, $query);
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                  </div>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" required />
+        <input type="text" class="login-input" name="email" placeholder="Email Adress">
+        <input type="password" class="login-input" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Register" class="login-button">
+        <p class="link"><a href="login.php">Click to Login</a></p>
+    </form>
+<?php
+    }
+?>
+</body>
+</html>
